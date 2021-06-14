f7fb7b2f5915ad3ff669cfefc784a96a4be764ce
Min A <alsdk4556@gmail.com>
2021-06-14 11:15:33 +0900

    Add validation

--- main.py
+++ main.py
@@ -17,9 +17,32 @@ class WindowClass(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
 
-        self.pushButton.clicked.connect(self.set_file_path)
+        self.fileMsg = QMessageBox()
+        self.fileMsg.setWindowTitle("주의")
+        self.fileMsg.setIcon(QMessageBox.Warning)
+        self.fileMsg.setText("변환할 파일을 선택해주세요.")
+        self.fileMsg.setStandardButtons(QMessageBox.Yes)
+        self.fileMsg.setDefaultButton(QMessageBox.Yes)
+
+        self.dirMsg = QMessageBox()
+        self.dirMsg.setWindowTitle("주의")
+        self.dirMsg.setIcon(QMessageBox.Warning)
+        self.dirMsg.setText("디렉토리를 설정해주세요.")
+        self.dirMsg.setStandardButtons(QMessageBox.Yes)
+        self.dirMsg.setDefaultButton(QMessageBox.Yes)
+
+        self.noFileMsg = QMessageBox()
+        self.noFileMsg.setWindowTitle("주의")
+        self.noFileMsg.setIcon(QMessageBox.Warning)
+        self.noFileMsg.setText("분석할 파일이 없습니다.")
+        self.noFileMsg.setStandardButtons(QMessageBox.Yes)
+        self.noFileMsg.setDefaultButton(QMessageBox.Yes)
+
+        self.pushButton.clicked.connect(self.set_directory_path)
+        self.pushButton_4.clicked.connect(self.set_file_path)
         self.pushButton_2.clicked.connect(self.start_process)
         self.pushButton_3.clicked.connect(self.start_lda_process)
+
 
     def set_log(self, text):
         self.textEdit_2.append(text)
@@ -34,29 +57,42 @@ class WindowClass(QMainWindow, form_class):
     def set_file_path(self):
         file = QFileDialog.getOpenFileName(self, 'Load File', './', "*.pdf")
         if file[0]:
-            self.lineEdit.setText(file[0])
+            self.lineEdit_5.setText(file[0])
             print(file[0])
 
     def start_process(self):
-        file_path = self.lineEdit.text()
+        file_path = self.lineEdit_5.text()
         if(file_path != ''):
             self.textEdit_2.append('Start conversion processing ....')
-            self.workder = ConvertThread(file_path)
-            self.workder.finished.connect(self.set_text_convert)
-            self.workder.start()
+            self.convert = ConvertThread(file_path)
+            self.convert.finished.connect(self.set_text_convert)
+            self.convert.start()
+        else:
+            self.fileMsg.exec_()
+
 
     def start_lda_process(self):
         file_path = self.lineEdit.text()
-        if (file_path != ''):
-            file_list = glob.glob(os.path.dirname(file_path) + '/*')
-            max_features = int(self.lineEdit_2.text())
-            n_components = int(self.lineEdit_3.text())
-            n_words = int(self.lineEdit_4.text())
+        if file_path != '':
+            file_list = glob.glob(file_path + '/*')
+            count_file = 0
+            for file in file_list:
+                if file.endswith('_sentence.txt'):
+                    count_file += 1
 
-            self.textEdit_2.append('Start LDA processing ....')
-            self.lda = LDAThread(file_list, max_features, n_components, n_words)
-            self.lda.finished.connect(self.set_text_lda)
-            self.lda.start()
+            if count_file == 0:
+                self.noFileMsg.exec_()
+            else:
+                max_features = int(self.lineEdit_2.text())
+                n_components = int(self.lineEdit_3.text())
+                n_words = int(self.lineEdit_4.text())
+                self.textEdit_2.append('Start LDA processing ....')
+                self.lda = LDAThread(file_list, max_features, n_components, n_words)
+                self.lda.finished.connect(self.set_text_lda)
+                self.lda.start()
+        else:
+            self.dirMsg.exec_()
+
 
     @pyqtSlot(str, str, str)
     def set_text_convert(self, html_data, text_data, sentence_date):
@@ -118,5 +154,10 @@ if __name__ == "__main__" :
     app = QApplication(sys.argv)
     myWindow = WindowClass()
     myWindow.show()
+    # # LDA
+    # components, feature_names = LDA.do_lda('C:/Users/Min A/Desktop/nuga', 1000, 5)
+    # # topics
+    # topic_list = LDA.extract_topics(components, feature_names, 7)
+    # print('\n'.join(LDA.print_lda_process), '\n'.join(topic_list))
 
     app.exec_()
